432f770f98deadc248b5ad6caa2c98674169cff7
singh-ashok25 <62803698+singh-ashok25@users.noreply.github.com>
2020-05-23 23:53:04 +0100

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 yum install git -y
+sudo su - 
+git clone https://github.com/singh-ashok25/kubernates-ansible-aws.git
+curl https://raw.githubusercontent.com/ansible/ansible/stable-2.9/contrib/inventory/ec2.ini -o /etc/ansible/ec2.ini
